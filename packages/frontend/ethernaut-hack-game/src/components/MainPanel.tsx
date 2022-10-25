5f724b4fb875ca48b3cc14a35daa0d1486e0fc9c
kyrers <kyrers.eth@gmail.com>
2022-10-26 00:19:38 +0100

    Minor commit before computer hibernates...

--- packages/frontend/ethernaut-hack-game/src/components/MainPanel.tsx
+++ packages/frontend/ethernaut-hack-game/src/components/MainPanel.tsx
@@ -3,9 +3,9 @@ import { Row, Col } from "react-bootstrap";
 function MainPanel() {
     return (
         <Row className="main-panel">
-            <Col sm={4}>Challenge 1 </Col>
-            <Col sm={4}>Challenge 2</Col>
-            <Col sm={4}>Challenge 3</Col>
+            <Col sm={4} className="challenge-card">Challenge 1</Col>
+            <Col sm={4} className="challenge-card">Challenge 2</Col>
+            <Col sm={4} className="challenge-card">Challenge 3</Col>
         </Row >
     );
 }
