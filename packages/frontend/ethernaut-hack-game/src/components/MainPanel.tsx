fd80330debd2c84f829d3754b19565d0ee9b67e5
kyrers <kyrers.eth@gmail.com>
2022-12-15 01:20:40 +0000

    UI improvements

--- packages/frontend/ethernaut-hack-game/src/components/MainPanel.tsx
+++ packages/frontend/ethernaut-hack-game/src/components/MainPanel.tsx
@@ -33,33 +33,57 @@ function MainPanel({ playerInfo, allowClicks, handleSelectedChallenge }: Functio
                                  RENDER
     --------------------------------------------------------------*/
     const renderChallenges = () => {
-        const renderRow = (challengesRow: Array<any>, index: Number) => {
+        const splitRowIntoChunks = (row: Array<any>, chunkSize: number) => {
+            let sortedCategory = [];
+            for (let i = 0; i < row.length; i += chunkSize) {
+                const chunk = row.slice(i, i + chunkSize);
+
+                //Fill remaining positions so it's easier to position correctly
+                const diff = chunkSize - chunk.length;
+                for (let x = 0; x < diff; x++) {
+                    chunk.push(null);
+                }
+
+                sortedCategory.push(chunk);
+            }
+
+            return sortedCategory;
+        };
+
+        const renderChallengeCol = (challenge: any, index: Number) => {
+            if (null === challenge) {
+                return (<Col key={`visual-helper-${index}`} sm={2} />);
+            }
+
             return (
-                <Row key={`challenge-row-${index}`}>
-                    {
-                        challengesRow.map(challenge =>
-                            <Col key={`challenge-id-${challenge.id}`} sm={2}
-                                className={`challenge-card ${hasSolvedChallenge(challenge.id) ? "solved-background" : "unsolved-background"} ${allowClicks ? "" : "pointer-events-none"}`}
-                                onClick={() => handleColClick(challenge)}>
-
-                                <span>{challenge.name}</span>
-
-                            </Col>
-                        )
-                    }
-
-                    {/* Workaround to keep challenge cards aligned in the last row (remove if new challenge is added) */
-                        index === 2 ?
-                            <Col key="visual-helper" sm={2} />
-                            :
-                            null
-                    }
-                </Row>
-            )
+                <Col key={`challenge-id-${challenge.id}`} sm={2}
+                    className={`challenge-card ${hasSolvedChallenge(challenge.id) ? "solved-background" : "unsolved-background"} ${allowClicks ? "" : "pointer-events-none"}`}
+                    onClick={() => handleColClick(challenge)}>
+                    <span>{challenge.name}</span>
+                </Col>
+            );
         }
 
+        const renderRow = (challengesCategory: Array<any>, index: Number) => {
+            let sortedCategory = splitRowIntoChunks(challengesCategory, 4);
+
+            return (
+                sortedCategory.map((challengesRow, index) => {
+                    return (
+                        <Row key={`challenge-row-${index}`}>
+                            {
+                                challengesRow.map((challenge, index) =>
+                                    renderChallengeCol(challenge, index)
+                                )
+                            }
+                        </Row>
+                    );
+                })
+            );
+        };
+
         return (
-            data.challenges.map((challengesRow, index) => renderRow(challengesRow, index))
+            data.challenges.map((challengesCategory, index) => renderRow(challengesCategory, index))
         );
     }
 
