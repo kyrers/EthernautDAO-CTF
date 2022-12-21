804d71d1f6661a9bc50b2eecb66c544bdb732ba6
kyrers <kyrers.eth@gmail.com>
2022-12-21 21:54:29 +0000

    Improved help text

--- packages/frontend/ethernaut-hack-game/src/utils/strings.ts
+++ packages/frontend/ethernaut-hack-game/src/utils/strings.ts
@@ -1,5 +1,9 @@
 export const strings = {
     "helpTitle": "How it works",
-    "helpText": "1. Choose a challenge. \n\n2. Create an instance of that challenge and solve it. \n\n3. Validate your solution. \n\n\nNote that you are not told what to do. Part of the challenge is figuring out what the exploit is. \nAll you have access to are contract addresses and code.",
+    "helpText": "1. Choose a challenge. \n\n" + 
+    "2. Create an instance of that challenge and solve it. \n\n" + 
+    "3. Validate your solution. \n\n" + 
+    "For each challenge, you can see all relevant contracts code. You also have access to the main contract address. All contracts are verified on goerli-optimism.etherscan in case you need more information. \n\n" +
+    "Note that you are not told what to do. Part of the challenge is figuring out what the exploit is.",
     "footerText": "EthernautDAO 2022✧"
 };
